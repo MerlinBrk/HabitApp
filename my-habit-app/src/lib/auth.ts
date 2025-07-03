837584f3bf5c66064285c14a8cb135f28dc54279
MerlinBrk <merlin.buerkle@gmail.com>
2025-07-03 07:41:47 +0200

    Update auth.ts

--- my-habit-app/src/lib/auth.ts
+++ my-habit-app/src/lib/auth.ts
@@ -5,10 +5,26 @@ export async function syncUserIdToLocalStorage() {
   const { data, error } = await supabase.auth.getUser();
   if (error || !data.user) return;
 
-  const userName = data.user.user_metadata?.display_name;
+  
   const userId = data.user.id;
   const userEmail = data.user.email;
+
+  const { data: profile, error: profileError } = await supabase
+    .from("Profiles")
+    .select("username")
+    .eq("id", data.user.id)
+    .maybeSingle();
+
+  if (profileError) {
+    console.error("Fehler beim Laden des Profils:", profileError.message);
+    return null;
+  }
+
+  if (!profile) {
+    console.log("Kein Profil gefunden");
+    return null;
+  }
   localStorage.setItem('user_id', userId);
-  localStorage.setItem('user_name',userName);
+  localStorage.setItem('user_name',profile.username);
   localStorage.setItem('user_email',userEmail);
 }
