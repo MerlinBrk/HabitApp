cc5bf6d4ea2fd4fb9aa4dfe7139bf84d2dc99b1d
MerlinBrk <merlin.buerkle@gmail.com>
2025-07-04 16:43:18 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/HabitApp/sw.js', { scope: '/HabitApp/' })})}
